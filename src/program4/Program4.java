961261f3fefe865165ff8fb6e57f7c579362aaf3
Connor Ludwig <connorlud@gmail.com>
2018-04-24 22:11:41 -0600

    Program4 Commit

---
+++ src/program4/Program4.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program4;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Connor
+ */
+public class Program4 {
+
+    public static ArrayList<Integer> greedy(int[] coins, int total){
+            //catches exception if array is empty
+            if(coins.length == 0) {
+		throw new IllegalArgumentException("Array of size 0 is not allowed");
+            }
+            ArrayList<Integer> list = new ArrayList<>(); //creates an array list to hold coin amounts
+                for(int i = coins.length - 1; i>=0; i--){ //for loops that starts at the far right of the array
+                    if(coins[i] <= total){ //enters if statment if the value of coin in index i is equal to or less than amount
+                        total = total - coins[i]; //subtracts coin amount from total
+                        list.add(coins[i]); //adds coin denomination to array list
+                        if(total == 0){ //if total is equal to zero, the algorthim is complete so it returns the list
+                            return list;
+                        }
+                        else{ //if the total is not 0 more coins must be chosen
+                        i++; //starts the array over again at current coin value it was at to check to see if it can be used again
+                    }
+                  }
+                }
+            
+             return list; //returns list
+        }
+    
+}
---
+++ test/program4/Program4Test.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program4;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Connor
+ */
+public class Program4Test {
+    
+ @Test
+    public void testMethod() {
+        int array[] = {1,5,10,25};
+        ArrayList<Integer> coins = new ArrayList<Integer>();
+        coins.add(25);
+        assertEquals(coins, Program4.greedy(array, 25));
+    }
+    
+     @Test
+    public void testMethod2() {
+        int array[] = {1,7,8,33};
+        ArrayList<Integer> coins = new ArrayList<Integer>();
+        coins.add(33);
+        coins.add(33);
+        coins.add(1);
+        coins.add(1);
+        coins.add(1);
+        coins.add(1);
+        coins.add(1);
+        coins.add(1);
+        assertEquals(coins, Program4.greedy(array, 72));
+    }
+    
+    
+     @Test
+    public void testMethod3() {
+        int array[] = {1,5,7,8,9};
+        ArrayList<Integer> coins = new ArrayList<Integer>();
+        coins.add(9);
+        coins.add(9);
+        coins.add(1);
+        coins.add(1);
+        assertEquals(coins, Program4.greedy(array, 20));
+    }
+    
+    @Test
+    public void testMethod4(){
+        int array[] = {};
+        boolean thrown = false;
+        try {
+        Program4.greedy(array, 0);
+        } 
+        catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        
+    assertTrue(thrown);
+  }
+    
+}
